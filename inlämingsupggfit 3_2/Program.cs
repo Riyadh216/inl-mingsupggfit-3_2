1f2e2c991136c0666a3278cbe65383c1354f2df6
Admin <Admin@TEB123020>
2023-10-20 15:33:56 +0200

    Add project files.

---
+++ inlämingsupggfit 3_2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Inlämningsuppgift_3._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Har du gått ut gymnasiet? (j/n)");
+            string gymnasiet = Console.ReadLine();
+            Console.WriteLine("Hur gammal är du?");
+            int ålder = int.Parse(Console.ReadLine());
+            if (gymnasiet == "j" && ålder < 22)
+            {
+                Console.WriteLine("Vi vill gärna anställa dig!");
+            }
+            else
+            {
+                Console.WriteLine("Vi letar tyvärr efter annan personal just nu.");
+            }
+        }
+    }
+}
